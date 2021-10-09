b9fee38076bca969bb6e87c87e9d0833755524d3
MaarufB <maarufburad1231@gmail.com>
2021-10-10 03:07:03 +0800

    Add database and view

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace MyCoreMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category{ get; set; }
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCoreMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } // This will become our primary key for our Category table in Database
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
